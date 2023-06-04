0459867cc463bee997e6ea0e21d84a0bcba9f2ec
Michael Südkamp <ms2702@online.de>
2023-06-04 10:31:06 +0200

    app.ts working

--- app.ts
+++ app.ts
@@ -17,12 +17,14 @@ app.set('view engine', 'ejs');
 
 // database connection
 const dbURI = process.env.MONGODB_URL;
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000))
-  .catch((err) => console.log(err));
+mongoose.set("strictQuery", false);
+// mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+mongoose.connect(dbURI)
+  .then((result: any) => app.listen(3000))
+  .catch((err: any) => console.log(err));
 
 // routes
 app.get('*', checkUser);
-app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
+app.get('/', (req: any, res: any) => res.render('home'));
+app.get('/smoothies', requireAuth, (req: any, res: any) => res.render('smoothies'));
 app.use(authRoutes);
